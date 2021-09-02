78cdf7b6214e765c0f925e21215109c41259b1dd
ryumtym <mtymryu@gunma-u.ac.jp>
2021-09-02 16:01:42 +0900

    update
    update

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
-
-
-router.post('/', function(req, res, next){
-
-
-
+router.get('/', function(req, res, next){
   res.header('Content-Type', 'application/json; charset=utf-8')
-  res.send(result);
+  res.send("HELLO WORLD!");
 });
 module.exports = router;
